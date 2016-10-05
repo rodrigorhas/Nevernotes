af7afb5afc33ff10418e7b79e09edb275be17305
rodrigorhas <rodrigorhas@gmail.com>
2016-10-04 22:33:34 -0300

    Menu da nota adicionado

--- app/js/app.js
+++ app/js/app.js
@@ -1,43 +1,43 @@
 angular.module("App", ['ngStorage'])
 
-	.filter("filterByTags", function () {
-		return function (items, tags) {
-			if(tags.length)
-				return items.filter(function (item, index) {
+.filter("filterByTags", function () {
+	return function (items, tags) {
+		if(tags.length)
+			return items.filter(function (item, index) {
 
-					var match = false;
+				var match = false;
 
-					item.tags.forEach(function (tag, tagIndex) {
-						tags.forEach(function (searchTag) {
-							if(tag.name.indexOf(searchTag.slice(1)) > -1)
-								match = true;
-						});
+				item.tags.forEach(function (tag, tagIndex) {
+					tags.forEach(function (searchTag) {
+						if(tag.name.indexOf(searchTag.slice(1)) > -1)
+							match = true;
 					});
-
-					return match;
 				});
-			else
-				return items;
-		}
-	})
 
-	.run(function ($localStorage) {
-		if(!$localStorage["nevernotes-store"]) {
-			$localStorage["nevernotes-store"] = [];
-		}
-	})
+				return match;
+			});
+		else
+			return items;
+	}
+})
 
-	.controller("Main", function ($scope, $timeout, $localStorage) {
+.run(function ($localStorage) {
+	if(!$localStorage["nevernotes-store"]) {
+		$localStorage["nevernotes-store"] = [];
+	}
+})
 
-		$scope.loadStore = function () {
-			$scope.store = $localStorage["nevernotes-store"];
-		}
+.controller("Main", function ($scope, $timeout, $localStorage) {
 
-		$scope.loadStore();
+	$scope.loadStore = function () {
+		$scope.store = $localStorage["nevernotes-store"];
+	}
 
-		function randomHash () {
-			return Math.random().toString(36).substring(2);
-		}
+	$scope.loadStore();
+
+	function randomHash () {
+		return Math.random().toString(36).substring(2);
+	}
 
 		// initialize tooltips
 		$timeout(function () { $('[data-toggle="tooltip"]').tooltip(); })
@@ -52,6 +52,7 @@ angular.module("App", ['ngStorage'])
 			id: "",
 			value: "",
 			addingPost: false,
+			enterOption: true,
 			tags: [{name: moment().format("L").replace(/\//g, '-')}],
 
 			tagInput: "",
@@ -104,9 +105,10 @@ angular.module("App", ['ngStorage'])
 
 			processInput: function (event) {
 				var self = this,
-					key = event.which || event.keyCode;
+				key = event.which || event.keyCode;
 
 				if(self.enterOption && key == 13) {
+					event.preventDefault();
 					self.save();
 				}
 			}
@@ -120,33 +122,16 @@ angular.module("App", ['ngStorage'])
 
 			str = "#" + str;
 
-			 var terms = $scope.search,
-			 	valid = true,
-			 	tags = $scope.getTags(terms);
+			var terms = $scope.search,
+			valid = true,
+			tags = $scope.getTags(terms);
 
 			for (var i = 0; i < tags.length; i++) {
-			 	var tag = tags[i];
-			 	if(tag == str) valid = false;
-			 };
+				var tag = tags[i];
+				if(tag == str) valid = false;
+			};
 
-			 if(valid) $scope.search = $scope.search + ($scope.search.length ? " " : "") + str;
-		}
-
-		$scope.removePost = function (index) {
-			$scope.store.splice(index, 1);
-		}
-
-		$scope.editPost = function (post, event) {
-			$scope.post.value = post.text;
-			$scope.post.tags = post.tags;
-			$scope.post.id = post.id;
-
-			// remove todas as classes note-edit antes de adicionar
-			// no target
-			$(".post").removeClass("note-edit");
-
-			// adiciona a classe note-edit no target
-			$(event.target).parents(".post").addClass("note-edit");
+			if(valid) $scope.search = $scope.search + ($scope.search.length ? " " : "") + str;
 		}
 
 		$scope.extractText = function (str) {
@@ -156,6 +141,68 @@ angular.module("App", ['ngStorage'])
 				})
 				.replace('#', '')
 				.trim();
+		}
+
+		$scope.NoteMenu = {
+			menu: $(".note-menu-list"),
+
+			currentPost: null,
+			event: null,
+			index: null,
+
+			hide: function () {
+				var self = this;
+
+				self.menu.css({display: "none"});	
+			},
+
+			reset: function () {
+				var self = this;
+
+				self.event = null;
+				self.currentPost = null;
+				self.index = null;
+			},
+
+			position: function (event, post, index) {
+				var self = this,
+				target = $(event.target),
+				button = (target.parents('.btn').length) ? target.parents('.btn') : target,
+				btnOffset = button.offset();
+
+				self.reset();
+
+				self.event = event;
+				self.currentPost = post;
+				self.index = index;
+
+				console.log(index);
+
+				self.menu.css({top: btnOffset.top, left: ((btnOffset.left - 225) + 32), display: "block"});
+			},
+
+			options: {
+				Edit: function () {
+					var self = $scope.NoteMenu;
+
+					$scope.post.value = self.currentPost.text;
+					$scope.post.tags = self.currentPost.tags;
+					$scope.post.id = self.currentPost.id;
+
+					// remove todas as classes note-edit antes de adicionar
+					// no target
+					$(".post").removeClass("note-edit");
+
+					// adiciona a classe note-edit no target
+					$(self.event.target).parents(".post").addClass("note-edit");
+				},
+
+				Delete: function () {
+					var self = $scope.NoteMenu;
+
+					$scope.store.splice(self.index, 1);
+				}
+			}
 		}
 
 	});
