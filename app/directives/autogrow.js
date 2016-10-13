1c3412538118fc160d585bab0e75e7ce411d06e1
rodrigorhas <rodrigorhas@gmail.com>
2016-10-13 01:45:47 -0300

    diretiva nao usada removida

--- app/directives/autogrow.js
+++ app/directives/autogrow.js
@@ -6,25 +6,4 @@ angular.module("App")
 			$timeout(() => element.autosize());
 		}
 	}
-}])
-.directive('contenteditable', function() {
-    return {
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            // view -> model
-            elm.bind('blur', function() {
-                scope.$apply(function() {
-                    ctrl.$setViewValue(elm.html());
-                });
-            });
-
-            // model -> view
-            ctrl.$render = function() {
-                elm.html(ctrl.$viewValue);
-            };
-
-            // load init value from DOM
-            ctrl.$setViewValue(elm.html());
-        }
-    };
-});
+}]);
